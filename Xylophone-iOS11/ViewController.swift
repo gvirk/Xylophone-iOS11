b623d0606ef21c25f189345b3fa5b1e9c3c6f499
Gursharnbir Singh <gursharnbirsingh@gursharnbirsmbp>
2018-07-09 16:10:59 -0600

    assign all tag's to all wav files. baby step complete

--- Xylophone-iOS11/ViewController.swift
+++ Xylophone-iOS11/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController{
     
+    var player: AVAudioPlayer?
+     var selectedSoundFileName : String = ""
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +23,32 @@ class ViewController: UIViewController{
     
     
     @IBAction func notePressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
         
+        playSound(selectedSound : selectedSoundFileName)
         
+    }
+    
+    func playSound( selectedSound : String){
+        guard let url = Bundle.main.url(forResource: selectedSound, withExtension: "wav") else { return }
         
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
     
